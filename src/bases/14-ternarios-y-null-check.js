39f242c66edc8faa0469e3f34be2c0f12c22561b
Daniel Verdú <verdudaniel7@gmail.com>
2021-08-09 09:39:26 +0200

    reforzamiento js

---
+++ src/bases/14-ternarios-y-null-check.js
@@ -0,0 +1,14 @@
+let firstName;
+let lastName;
+
+//firstName = 'Daniel'
+
+// || checkea si el valor es null o undefined
+console.log(`${firstName || 'No firstName'} ${lastName || 'No LastName'}` )
+
+
+const isActive = true
+
+const mssg = (isActive) ? 'Activo' : 'Inactivo'
+
+console.log(mssg)
